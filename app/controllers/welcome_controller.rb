7f6070b87268624a29087fe9a9ea752e5acf2de1
Jeff Reed <jcreed063@gmail.com>
2019-03-17 13:42:42 -0400

    Tablize the display of slime recipe links
    List the slimes and loop thru to create links

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,30 @@
 class WelcomeController < ApplicationController
   layout "application"
 
+  before_action :set_slimes, only: [:index]
+
   def index
+    set_slimes
+  end
+
+  private
+
+  def set_slimes
+    @slimes = %w(
+      Basic
+      Butter
+      Clear
+      Cloud
+      Cloud\ Cream
+      Crunchy
+      Fluffy
+      Glitter
+      Glossy
+      Jiggly
+      Pigmented
+      Sticky
+      Stretchy
+      Thick
+    )
   end
 end
